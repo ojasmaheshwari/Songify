8ae720944bf0b2b3ffeb01c2fe7cc634d491890e
Ojas Maheshwari <ojasmaheshwari2006@gmail.com>
2025-04-05 22:21:54 +0530

    implement navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Navbar from "./components/navbar";
+
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar />
     </>
   );
 }
